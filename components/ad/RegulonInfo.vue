e9fe8f72d6afadf7561f5687a4a95d3d1df60a38
Cankun Wang <flykun0620@gmail.com>
2020-06-04 10:33:25 -0400

    Add regulon info

---
+++ components/ad/RegulonInfo.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-hover v-slot:default="{ hover }" open-delay="0">
+    <v-card
+      class="mx-auto"
+      :elevation="hover ? 10 : 2"
+      :class="{ 'on-hover': hover }"
+    >
+      <v-card-title class="primary white--text subtitle-1"
+        >Cell-type-specific regulon inference</v-card-title
+      >
+
+      <v-card-text>
+        <v-row
+          ><v-col xs="12" md="12" lg="12">
+            <v-text-field
+              v-model="search"
+              prepend-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+            <v-data-table
+              dense
+              :search="search"
+              :headers="headers"
+              :items="regulon"
+              :items-per-page="10"
+              class="elevation-1"
+            >
+            </v-data-table>
+            <iframe
+              src="https://bmbl.bmi.osumc.edu/iris3/results.php?jobid=20200520163920"
+              height="800"
+              width="100%"
+            ></iframe>
+            <p class="text--primary ma-4">
+              <span class="font-weight-bold"
+                ><a
+                  href="https://bmbl.bmi.osumc.edu/iris3/results.php?jobid=20200520163920"
+                  target="_blank"
+                  style="text-decoration:none;"
+                >
+                  Open regulon result page in new tab</a
+                ></span
+              >
+            </p>
+          </v-col></v-row
+        >
+      </v-card-text>
+    </v-card>
+  </v-hover>
+</template>
+
+<script>
+import { mapState } from 'vuex' // <--- To map data from Vuex
+
+export default {
+  name: 'RegulonInfo',
+  components: {},
+  props: {},
+
+  data() {
+    return {
+      search: '',
+      headers: [
+        { text: 'Index', value: 'index' },
+        { text: 'Transcription factor', value: 'tf_name' },
+        { text: 'Regulon specificity score', value: 'rss' },
+        { text: 'Regulon p-value', value: 'rss_pval' },
+        { text: 'Genes', value: 'gene_symbol' }
+      ]
+    }
+  },
+  computed: {
+    ...mapState({
+      regulon: (state) => state.ad.regulon
+    })
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
