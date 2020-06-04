a7160190f274c16a87c056bf64d006a744b5cb34
Cankun Wang <flykun0620@gmail.com>
2020-06-04 10:34:52 -0400

    Add enrichment table component

---
+++ components/ad/EnrichmentTable.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card class="mx-auto pa-2" max-width="400" tile> </v-card>
+</template>
+
+<script>
+export default {
+  name: 'MotifInfo',
+  props: {
+    enrich: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
