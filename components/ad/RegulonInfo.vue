1f0f992983928f89a56c89e7af1377ef91c0351a
Cankun Wang <flykun0620@gmail.com>
2020-06-17 21:48:51 -0400

    Remove card title

--- components/ad/RegulonInfo.vue
+++ components/ad/RegulonInfo.vue
@@ -1,54 +1,47 @@
 <template>
-  <v-hover v-slot:default="{ hover }" open-delay="0">
-    <v-card
-      class="mx-auto"
-      :elevation="hover ? 10 : 2"
-      :class="{ 'on-hover': hover }"
-    >
-      <v-card-title class="primary white--text subtitle-1"
-        >Cell-type-specific regulon inference</v-card-title
-      >
-
-      <v-card-text>
-        <v-row
-          ><v-col xs="12" md="12" lg="12">
-            <v-text-field
-              v-model="search"
-              prepend-icon="mdi-magnify"
-              label="Search"
-              single-line
-              hide-details
-            ></v-text-field>
-            <v-data-table
-              dense
-              :search="search"
-              :headers="headers"
-              :items="regulon"
-              :items-per-page="10"
-              class="elevation-1"
-            >
-            </v-data-table>
-            <iframe
-              src="https://bmbl.bmi.osumc.edu/iris3/results.php?jobid=20200520163920"
-              height="800"
-              width="100%"
-            ></iframe>
-            <p class="text--primary ma-4">
-              <span class="font-weight-bold"
-                ><a
-                  href="https://bmbl.bmi.osumc.edu/iris3/results.php?jobid=20200520163920"
-                  target="_blank"
-                  style="text-decoration:none;"
-                >
-                  Open regulon result page in new tab</a
-                ></span
+  <v-card>
+    <v-card-text>
+      <v-row
+        ><v-col xs="12" md="12" lg="12">
+          <v-text-field
+            v-model="search"
+            prepend-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+          <v-btn color="primary">
+            Download
+          </v-btn>
+          <v-data-table
+            dense
+            :search="search"
+            :headers="headers"
+            :items="regulon"
+            :items-per-page="10"
+            class="elevation-1"
+          >
+          </v-data-table>
+          <iframe
+            src="https://bmbl.bmi.osumc.edu/iris3/results.php?jobid=20200520163920"
+            height="800"
+            width="100%"
+          ></iframe>
+          <p class="text--primary ma-4">
+            <span class="font-weight-bold"
+              ><a
+                href="https://bmbl.bmi.osumc.edu/iris3/results.php?jobid=20200520163920"
+                target="_blank"
+                style="text-decoration:none;"
               >
-            </p>
-          </v-col></v-row
-        >
-      </v-card-text>
-    </v-card>
-  </v-hover>
+                Open cell-type-specific regulon result page in new tab</a
+              ></span
+            >
+          </p>
+        </v-col></v-row
+      >
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
