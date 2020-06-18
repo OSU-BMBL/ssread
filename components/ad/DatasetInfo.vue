25e270f8cf74425848505f3fbf1063baaa0b1818
Cankun Wang <flykun0620@gmail.com>
2020-06-17 21:47:46 -0400

    Update line height and data table contents

--- components/ad/DatasetInfo.vue
+++ components/ad/DatasetInfo.vue
@@ -5,112 +5,198 @@
       :elevation="hover ? 10 : 2"
       :class="{ 'on-hover': hover }"
     >
-      <v-card-title class="primary white--text subtitle-1"
-        >Dataset information</v-card-title
+      <v-card-title class="primary white--text title-1"
+        >General information</v-card-title
       >
       <v-card-text> </v-card-text>
 
       <v-layout row wrap>
-        <v-flex xs12 md12 lg7
-          ><v-card-title class="my-0 py-0"
-            >Overview<v-divider class="my-0 py-0"></v-divider
+        <v-flex xs12 md12 lg5
+          ><v-card-title class="my-2 py-0 subtitle-1 font-weight-bold"
+            >Overview<v-divider class="my-2 py-0"></v-divider
           ></v-card-title>
 
           <v-card-text>
-            <p class="my-0">
+            <p class="my-2">
               <span class="text--secondary">scREAD Data ID: </span>
               <span class="text--primary">{{ dataset[0].data_id }}</span>
             </p>
-            <p class="my-0">
+            <p class="my-2">
               <span class="text--secondary">Species: </span>
               <span class="text--primary">{{ dataset[0].species }}</span>
             </p>
-            <p class="my-0">
+            <p class="my-2">
               <span class="text--secondary">Region: </span>
               <span class="text--primary">{{ dataset[0].region }}</span>
             </p>
-            <p class="my-0">
+            <p class="my-2">
               <span class="text--secondary">Condition: </span>
               <span class="text--primary">{{ dataset[0].condition }}</span>
             </p>
-            <p class="my-0">
+            <p class="my-2">
               <span class="text--secondary">Stage: </span>
               <span class="text--primary">{{ dataset[0].stage }}</span>
             </p>
-            <p class="my-0">
+            <p class="my-2">
               <span class="text--secondary">Gender: </span>
               <span class="text--primary">{{ dataset[0].gender }}</span>
             </p>
-            <p class="my-0">
+            <p class="my-2">
               <span class="text--secondary">Age: </span>
               <span class="text--primary">{{ dataset[0].age }}</span>
             </p>
           </v-card-text>
-          <v-card-title class="my-0 py-0"
+          <v-card-title class="my-2 py-0 subtitle-1 font-weight-bold"
+            >Datasets from the same experiment
+            <v-divider class="my-2 py-0"></v-divider>
+          </v-card-title>
+          <v-card-text
+            ><span v-for="item in related" :key="item.id">
+              <a @click="handleClick(item.data_id)">{{ item.data_id }} </a>|
+            </span>
+          </v-card-text>
+          <v-dialog v-model="dialog" max-width="300">
+            <v-card>
+              <v-card-title class="my-2 py-0"
+                >Overview<v-divider class="my-2 py-0"></v-divider
+              ></v-card-title>
+              <v-card-text>
+                <p class="my-2">
+                  <span class="text--secondary">scREAD Data ID: </span>
+                  <span class="text--primary">{{
+                    computedDialogData.data_id
+                  }}</span>
+                </p>
+                <p class="my-2">
+                  <span class="text--secondary">Species: </span>
+                  <span class="text--primary">{{
+                    computedDialogData.species
+                  }}</span>
+                </p>
+                <p class="my-2">
+                  <span class="text--secondary">Region: </span>
+                  <span class="text--primary">{{
+                    computedDialogData.region
+                  }}</span>
+                </p>
+                <p class="my-2">
+                  <span class="text--secondary">Condition: </span>
+                  <span class="text--primary">{{
+                    computedDialogData.condition
+                  }}</span>
+                </p>
+                <p class="my-2">
+                  <span class="text--secondary">Braak stage: </span>
+                  <span class="text--primary">{{
+                    computedDialogData.stage
+                  }}</span>
+                </p>
+                <p class="my-2">
+                  <span class="text--secondary">Gender: </span>
+                  <span class="text--primary">{{
+                    computedDialogData.gender
+                  }}</span>
+                </p>
+                <p class="my-2">
+                  <span class="text--secondary">Age: </span>
+                  <span class="text--primary">{{
+                    computedDialogData.age
+                  }}</span>
+                </p>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="primary " text @click="openDetailsPage()">
+                  details </v-btn
+                ><v-btn color="grey darken-1" text @click="dialog = false">
+                  cancel
+                </v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-snackbar v-model="snackbar">
+            {{ text }}
+            <v-btn color="pink" text @click="snackbar = false">
+              Close
+            </v-btn>
+          </v-snackbar>
+        </v-flex>
+        <v-flex xs12 md12 lg7
+          ><v-card-title class="my-2 py-0 subtitle-1 font-weight-bold"
             >Dataset information
-            <v-divider class="my-0 py-0"></v-divider>
+            <v-divider class="my-2 py-0"></v-divider>
           </v-card-title>
           <v-card-text>
-            <p class="my-0">
+            <p class="my-2">
               <span class="text--secondary"
                 >Number of AD associated cells:
               </span>
-              <span class="text--primary">6377</span>
+              <span class="text--primary">{{ dataset[0].n_cell }}</span>
             </p>
-            <p class="my-0">
+            <p class="my-2">
               <span class="text--secondary"
-                >Number of cells from original dataset:
+                >Number of healthy-like cells:
               </span>
-              <span class="text--primary">6769</span>
+              <span class="text--primary">{{
+                dataset[0].n_original_cell - dataset[0].n_cell
+              }}</span>
             </p>
-            <p class="my-0">
-              <span class="text--secondary">Dataset source: </span>
-              <span class="text--primary"
-                >https://pubmed.ncbi.nlm.nih.gov/31042697/</span
-              >
-            </p>
-            <p class="my-0">
-              <span class="text--secondary">GSE number: </span>
-              <span class="text--primary">{{ dataset[0].geo_id }}</span>
-            </p>
-            <p class="my-0">
-              <span class="text--secondary">Pubmed ID: </span>
-              <span class="text--primary">31042697</span>
-            </p>
-            <p class="my-0">
-              <span class="text--secondary">Citation: </span>
-              <span class="text--primary"
-                >Mathys, H., Davila-Velderrain, J., Peng, Z. et al. Single-cell
-                transcriptomic analysis of Alzheimer’s disease. Nature 570,
-                332–337 (2019). https://doi.org/10.1038/s41586-019-1195-2
+            <p class="my-2">
+              <span class="text--secondary"
+                >Number of identified cell types:
               </span>
+              <span class="text--primary">{{ dataset[0].n_cluster }}</span>
             </p>
-          </v-card-text></v-flex
-        >
-        <v-flex xs12 md12 lg5
-          ><v-card-title class="my-0 py-0"
-            >Related dataset
-            <v-divider class="my-0 py-0"></v-divider>
-          </v-card-title>
-          <p class="my-0">
-            <span class="text--secondary">Same region: </span>
-            <span class="text--primary"
-              >AD00101: H-H-Prefrontal cortex-Male; AD00106: H-H-Prefrontal
-              cortex-Female</span
-            >
-          </p>
-          <p class="my-0">
-            <span class="text--secondary">Same stage: </span>
-            <span class="text--primary"
-              >AD00104: H-AD.early-Prefrontal cortex-Male_001</span
-            >
-          </p>
-          <p class="my-0">
-            <span class="text--secondary">Same gender: </span>
-            <span class="text--primary"
-              >AD00105: H-AD.early-Prefrontal cortex-Female_001</span
-            >
-          </p>
+
+            <div v-for="(pub, index) in publication" :key="pub.id">
+              <div v-if="publication.length === 1">
+                <div class="my-2 py-0 subtitle-2 font-weight-bold">
+                  Dataset source:
+                  <v-divider class="my-2 py-0"></v-divider>
+                </div>
+              </div>
+              <div v-else>
+                <div class="my-2 py-0 subtitle-2 font-weight-bold">
+                  Dataset source {{ index + 1 }}:
+                  <v-divider class="my-2 py-0"></v-divider>
+                </div>
+              </div>
+
+              <p class="my-2">
+                <span class="text--secondary">Title: </span>
+                <span class="text--primary">{{ pub.title }} </span>
+              </p>
+              <p class="my-2">
+                <span class="text--secondary">Methodology: </span>
+                <span class="text--primary">{{ pub.methodology }}</span>
+              </p>
+              <p class="my-2">
+                <span class="text--secondary">Protocol: </span>
+                <span class="text--primary">{{ pub.protocol }}</span>
+              </p>
+              <p class="my-2">
+                <span class="text--secondary">GEO/synapse number: </span>
+                <span class="text--primary">{{
+                  pub.public_id.split(';')[index]
+                }}</span>
+              </p>
+              <p class="my-2">
+                <span class="text--secondary">Pubmed ID: </span>
+                <span class="text--primary">
+                  <a
+                    :href="'https://pubmed.ncbi.nlm.nih.gov/' + pub.pmid"
+                    target="_blank"
+                    style="text-decoration:none;"
+                    >{{ pub.pmid }}</a
+                  ></span
+                >
+              </p>
+              <p class="my-2">
+                <span class="text--secondary">Citation: </span>
+                <span class="text--primary">{{ pub.citation }} </span>
+              </p>
+            </div></v-card-text
+          >
         </v-flex>
       </v-layout>
     </v-card>
@@ -118,12 +204,65 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   name: 'DasetInfo',
   props: {
     dataset: {
       type: Array,
       required: true
+    },
+    related: {
+      type: Array,
+      required: true
+    },
+    publication: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      dialog: false,
+      computedDialogData: [],
+      snackbar: false,
+      text: "Hello, I'm a snackbar",
+      search: '',
+      headers: [
+        {
+          text: 'scREAD data ID',
+          align: 'start',
+          sortable: false,
+          value: 'data_id'
+        },
+        { text: 'Gender', value: 'gender' },
+        { text: 'Condition', value: 'condition' },
+        { text: 'Braak stage', value: 'stage' },
+        { text: 'Age', value: 'age' },
+        { text: 'Mice model', value: 'mice_model' },
+        { text: 'GEO/synapse ID', value: 'public_id' },
+        { text: '#Cells', value: 'n_cell' }
+      ]
+    }
+  },
+  computed: {
+    ...mapState({
+      dialogData: (state) => state.ad.dialogDataset
+    })
+  },
+  methods: {
+    openPage(id) {
+      this.$router.push(id)
+    },
+    async handleClick(id) {
+      // this.$router.push('/browse/' + dataset.data_id)
+      // console.log(id)
+      await this.$store.dispatch('ad/setDialog', id)
+      this.computedDialogData = this.dialogData[0]
+      this.dialog = true
+    },
+    openDetailsPage() {
+      this.$router.push(this.computedDialogData.data_id)
     }
   }
 }
