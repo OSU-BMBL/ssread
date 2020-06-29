472cb235a751eee788e5781d6acb60cf66517fd0
Cankun Wang <flykun0620@gmail.com>
2020-06-28 22:18:49 -0400

    Add loading progress circle

--- components/utils/Loading.vue
+++ components/utils/Loading.vue
@@ -1,13 +1,21 @@
 <template lang="html">
-  <div class="loading-page" v-if="loading">
-    <h1>Loading...</h1>
+  <div v-if="loading" class="loading-page">
+    <h2>Loading...</h2>
+    <v-progress-circular
+      width="6"
+      color="primary"
+      style="color: #6A4C9C; width: 80px; height: 80px;"
+      indeterminate
+    ></v-progress-circular>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    loading: false
+    loading: false,
+    progress: 0,
+    error: ''
   }),
   methods: {
     start() {
@@ -15,6 +23,9 @@ export default {
     },
     finish() {
       this.loading = false
+    },
+    fail(error) {
+      this.error = error
     }
   }
 }
@@ -27,11 +38,12 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(255, 255, 255, 0.8);
+  background: rgba(255, 255, 255, 0.1);
   text-align: center;
   padding-top: 200px;
-  font-size: 30px;
-  font-family: sans-serif;
+  font-size: 40px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   z-index: 1000;
 }
 </style>
