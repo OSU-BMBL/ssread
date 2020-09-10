a8a1d661f5112fa734e2651b0eb12ddecd9da636
Cankun Wang <flykun0620@gmail.com>
2020-09-10 00:10:49 -0400

    Update pm2 settings for production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'Scread database',
+      name: 'scREAD frontend',
       script: './node_modules/nuxt-start/bin/nuxt-start.js',
       port: 9003,
       watch: '.',
@@ -9,22 +9,8 @@ module.exports = {
       max_memory_restart: '5G',
       log_date_format: 'YYYY-MM=DD HH:mm:ss',
       env: {
-        NODE_ENV: 'production',
-      },
-    },
-  ],
-
-  deploy: {
-    production: {
-      user: 'SSH_USERNAME',
-      host: 'SSH_HOSTMACHINE',
-      ref: 'origin/master',
-      repo: 'GIT_REPOSITORY',
-      path: 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy':
-        'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
-    },
-  },
+        NODE_ENV: 'production'
+      }
+    }
+  ]
 }
